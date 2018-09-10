16218be20e34a2358739830c109c76bab9db938d
lbjgoat0808 <42793305+lbjgoat0808@users.noreply.github.com>
2018-09-10 12:06:32 +0800

    Add files via upload

---
+++ VSA Y10 Coding/p5jsTutorial1.1-master/sketch.js
@@ -0,0 +1,56 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(255,255,255)
+
+  // change the paint brush to a specific color according to RGB values
+  fill(255,0,0);
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  rect(0,210,1000,10)
+
+  fill (255,0,0)
+
+  rect(0,240,1000,10)
+
+  fill (255,0,0)
+
+  rect(0,180,1000,10)
+
+  fill (255,0,0)
+
+  rect(0,150,1000,10)
+
+  fill (255,0,0)
+
+    rect(340,120,370,10)
+
+  fill (255,0,0)
+
+      rect(340,90,370,10)
+
+  fill (255,0,0)
+
+    rect(340,60,370,10)
+
+  fill (255,0,0)
+
+
+
+
+
+
+
+
+
+
+
+    
+}
